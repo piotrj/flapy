e9a3855c7bafddc1c153daeda6bba9232eadf533
Piotr Jakubowski <piotrj@gmail.com>
2010-01-11 19:27:40 +0100

    Added login and post comments view

---
+++ flaker/__init__.py
@@ -0,0 +1,351 @@
+# -*- coding: utf-8 -*-
+
+import urllib2
+from datetime import datetime
+import time
+try:
+    import json
+except ImportError:
+    import simplejson as json
+import base64
+from poster.streaminghttp import register_openers
+from poster.encode import multipart_encode
+
+register_openers()
+
+
+
+DEBUG=False
+
+class FlakError(Exception):
+    "Basic Flaker error."
+    pass
+
+class FlakAuthorizationError(FlakError):
+    pass
+
+class FlakDuplicateMessageError(FlakError):
+    pass
+
+class FlakConfigurationError(FlakError):
+    """This error is raised in situations when the user hasn't
+    provided enough data to do perform an operation.
+
+    """
+    pass
+
+def login_required(fun):
+    def _wrapper(self, *a, **kw):
+        if not (self.login and self.password):
+            raise FlakConfigurationError("You must provide some credentials in order to perform this action.")
+        else:
+            return fun(self, *a, **kw)
+
+    return _wrapper
+
+def flak_decoding(fun):
+    def wrapper(self, *a, **kw):
+        res = fun(self, *a, **kw)
+        if 'friends' in res:
+            return [self.flak_class(**e) for e in res['entries']], [self.flakuser_class(**e) for e in res['friends']]
+        else:
+            return [self.flak_class(**e) for e in res['entries']]
+    return wrapper
+
+class FlakUser(object):
+    """This class represents a user of Flaker.
+
+    """
+    def __init__(self, login=None, url=None, avatar=None, sex=None, action=None):
+        self.login = login
+        self.url = url
+        self.avatar = avatar
+        self.sex = sex # is this used anywhere?
+        self.action = action
+
+    def __eq__(self, other):
+        return self.login == other.login and self.url == other.url
+
+    def __repr__(self):
+        return "FlakUser(login=%r, url=%r, avatar=%r, sex=%r)" % (self.login, self.url, self.avatar, self.sex)
+
+class Flak(object):
+    """Basic message object."""
+    flakuser_class = FlakUser
+    def __init__(self,
+                 permalink=None,
+                 time=None,
+                 timestamp=None,
+                 comments=None,
+                 source=None,
+                 link=None,
+                 video=None,
+                 photo=None,
+                 user=None,
+                 text=None,
+                 has_photo=None,
+                 data=None,
+                 id=None,
+                 **kw):
+        self.permalink = permalink
+        self.video = video
+        self.comments = comments if not(comments) else [e if isinstance(e, Flak) else Flak(**e) for e in comments]
+        self.source = source
+        self.link = link
+        self.user = user if isinstance(user, FlakUser) else self.flakuser_class(**user)
+
+
+
+        self.text = text
+        self.photo = photo
+        self.data = data
+        self.id = id
+
+        # kludge, we are loosing tz information. note that datetime is
+        # in kw, not in normal args, because we don't want to shadow
+        # datetime
+        if isinstance(kw['datetime'], datetime):
+            self.datetime = kw['datetime']
+        else:
+            self.datetime = datetime.fromtimestamp(float(timestamp))
+    def __str__(self):
+        return ("%s@%s: %s" % (self.user.login, self.datetime, self.text)).encode('utf-8')
+
+    def __repr__(self):
+        return "Flak(permalink=%r, datetime=%r, comments=%r, source=%r, link=%r, video=%r, photo=%r, user=%r, text=%r, data=%r, id=%r)" % \
+            (self.permalink, self.datetime, self.comments,
+             self.source, self.link, self.video,
+             self.photo, self.user, self.text, self.data, self.id)
+
+
+
+class Flaker(object):
+
+    URI = 'http://api.flaker.pl/api/'
+    flak_class = Flak
+    flakuser_class = FlakUser
+
+
+    def __init__(self, login=None, password=None):
+        """If you want to perform actions requiring authorization
+        (eg. adding new messages), you must provide a `login` and a
+        `password`. You can always authorize yourself later with
+        `authorize`.
+
+        """
+        if login and password:
+            self.login, self.password = login, password
+        elif password:
+            raise FlakConfigurationError("If you provide a password you should also provide a login")
+        else:
+            self.login = self.password = None
+
+    def authorize(self, login=None, password=None):
+        """Authorize yourself with the given login and password."""
+        self.login, self.password = login, password
+
+    def _translate_value(self, val):
+        if isinstance(val, bool):
+            return "true" if val else "false"
+        else:
+            return val
+
+    def _request(self, data=None, authorize=False, **kw):
+
+
+        url = self.URI + '/'.join("%s:%s" % (k, self._translate_value(v)) for k, v in kw.iteritems())
+        if DEBUG:
+            print url
+        if data:
+            data, headers = multipart_encode(data)
+        else:
+            headers = {}
+        if authorize:
+            authheader = "Basic %s" % base64.encodestring("%s:%s"% (self.login, self.password))[:-1]
+            headers["Authorization"] =  authheader
+
+        req = urllib2.Request(url, data, headers)
+        try:
+            handle = urllib2.urlopen(req)
+        except urllib2.HTTPError, e:
+            code = e.getcode()
+            if code == 409:
+                raise FlakDuplicateMessageError
+            elif code == 401:
+                raise FlakAuthorizationError
+            elif code == 400:
+                raise FlakError(url)
+            else:
+                raise
+
+        try:
+            if DEBUG:
+                h = handle.read()
+                response = json.loads(h, object_hook=lambda d: dict((str(k), v) for k, v in d.iteritems()))
+            else:
+                response = json.load(handle, object_hook=lambda d: dict((str(k), v) for k, v in d.iteritems()))
+                h = None
+        except ValueError,e:
+            if DEBUG:
+                raise FlakError(h)
+            else:
+                raise FlakError
+
+        return response
+
+    @login_required
+    def auth(self):
+        """Check whether the authorization data is correct. Returns
+        True if such is the case.
+
+        """
+        r = self._request(type='auth', authorize=True)['status']
+        if r['text'] == "OK":
+            return True
+
+    @flak_decoding
+    def bookmarks(self, login):
+        """Retrieve all the Flaks bookmarked by `login`.
+        """
+        return self._request(login=login)
+
+    @login_required
+    def tags(self):
+        """Retrieve all tags observed by the authorized user."""
+        return self._request(type='tags', authorize=True, login=self.login)['tags']
+
+    def _bookmark(self, entry_id, bookmark):
+        action = 'set' if bookmark else 'unset'
+        r = self._request(type="bookmark", action=action, entry_id=entry_id, authorize=True)
+        if r['status']['text'] == 'OK':
+            return True
+        else:
+            raise FlakError(r)
+
+    @login_required
+    def bookmark(self, entry_id):
+        """Bookmark the entry whose id is provided."""
+        return self._bookmark(entry_id, True)
+
+    @login_required
+    def unbookmark(self, entry_id):
+        """Unbookmark the entry whose id is provided."""
+        return self._bookmark(entry_id, False)
+
+
+    def show(self, entry_id, comments = True, html = False):
+        """Return a single Flak identified by entry_id."""
+        return self.flak_class(**self._request(type='show', entry_id=entry_id, comments = comments, html = html)['entries'][0])
+
+    def _get_messages(self,
+                     tag=None,
+                     avatars='small',
+                     limit=20,
+                     from_=None,
+                     start=None,
+                     since=None,
+                     sort='desc',
+                     comments=False,
+                     **kw):
+        if tag:
+            if tag == True:
+                tag = 'all'
+            kw['tag'] = tag
+        kw['avatars'] = avatars
+        kw['limit'] = limit
+        if from_:
+            kw['from'] = from_
+        if start:
+            kw['start'] = start
+        if since:
+            if isinstance(since, datetime):
+                since = time.mktime(since.timetuple())
+            kw['since'] = since
+        kw['sort'] = sort
+        kw['comments'] = comments
+        if DEBUG:
+            print kw
+        return self._request(**kw)
+
+    @flak_decoding
+    def friends(self, login, since = None, html = False):
+        """Get all the friends of `login`.
+        """
+        
+        
+        return self._request(type='friends', login=login, since=since, html = html)
+
+    @flak_decoding
+    def query(self,
+              user=None,
+              site=None,
+              source=None,
+              **kw):
+        """Query Flaker for messages meeting certain criteria.
+
+        If you provide one of `user`, `site` or `source`, you will get
+        entries related to a given user, site or source. If you don't
+        specify any of them, you will get entries from the whole
+        "flakosphere".
+
+        Example values of `source` are:
+          * "favorites", "commented", "photos", "videos", "links" (names of streams)
+          * "flaker", "garnek", "fotka", "blox" (names of webservices).
+
+        Please consult http://blog.flaker.pl/api-pobieranie-wpisow/
+        for more specific information about possible values of
+        `source`.
+
+        Other arguments:
+
+         * `avatars` size of the avatar pictures. Possible values:
+           "small", "medium", "big" (default small).
+
+         * `limit` number of entries (default 20).
+
+         * `from_` the same as offset in SQL. Note the trailing "_".
+
+         * `start` retrieve entries whose id is bigger than `start`.
+
+         * `since` Unix timestamp or datetime.datetime object,
+           retrieve objects newer than `since`.
+
+         * `sort` sorting order: "asc" for ascending, "desc" for
+           descending.
+
+         * `comments` boolean, whether to retrieve comments (default
+           False).
+
+        """
+        if len([o for o in (user, site, source) if o]) > 1:
+            raise FlakConfigurationError("You may provide at most one of `user`, `site` and `source`.")
+        if user:
+            kw['type'] = 'user'
+            kw['login'] = user
+        elif site:
+            kw['type'] = 'site'
+            kw['site'] = site
+        elif source:
+            kw['type'] = 'source'
+            kw['source'] = source
+        else:
+            kw['type'] = 'flakosfera'
+        return self._get_messages(**kw)
+
+    @login_required
+    def submit(self, text=None, link=None, photo=None):
+        """Submit a message to Flaker.
+
+        `photo` may be either a file object (which will be uploaded)
+        or a url to some image in the Internet.
+        """
+        data = {'text': text}
+        if link:
+            data['link'] = link
+        if photo:
+            # if it's not a file assume it is a URL to one.
+            try:
+                data['photo'] = open(photo)
+            except IOError:
+                data['link'] = photo
+        return self._request(data=data, authorize=True, type='submit')['status']['info']
--- gui.py
+++ gui.py
@@ -10,33 +10,34 @@
 from PyQt4 import QtCore, QtWebKit, QtGui
 from PyQt4.QtGui import *
 import sys
-import flaker
-
+# import flaker
+from flaker import Flaker, Flak, FlakUser
 class EntryWidget(QWidget):
   def __init__(self, entry, parent = None):
     QWidget.__init__(self, parent)
 
+    self.entry = entry
     sizePolicy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
     self.setSizePolicy(sizePolicy)
     self.setObjectName("entryContainer")
 
-    self.entry = QWidget(self)
-    self.entry.setObjectName("entry")
-    self.avatar = QtWebKit.QWebView(self.entry)
+    self.entryWidget = QWidget(self)
+    self.entryWidget.setObjectName("entry")
+    self.avatar = QtWebKit.QWebView(self.entryWidget)
     self.avatar.setGeometry(QtCore.QRect(15, 15, 50, 50))
     sizePolicy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
     self.avatar.setSizePolicy(sizePolicy)
-    self.avatar.setUrl(QtCore.QUrl(entry["user"]["avatar"]))
+    self.avatar.setUrl(QtCore.QUrl(entry.user.avatar))
     self.avatar.setObjectName("avatar")
 
 
-    self.verticalLayoutWidget = QWidget(self.entry)
+    self.verticalLayoutWidget = QWidget(self.entryWidget)
     sizePolicy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
     self.verticalLayoutWidget.setSizePolicy(sizePolicy)
     self.verticalLayoutWidget.move(70, 10)
-    self.verticalLayoutWidget.setMinimumSize(QtCore.QSize(300, 100))
-    self.verticalLayoutWidget.setSizeIncrement(QtCore.QSize(0, 10))
-    self.verticalLayoutWidget.setBaseSize(QtCore.QSize(300, 100)) 
+    self.verticalLayoutWidget.setMinimumSize(QtCore.QSize(400, 100))
+    # self.verticalLayoutWidget.setSizeIncrement(QtCore.QSize(0, 10))
+    self.verticalLayoutWidget.setBaseSize(QtCore.QSize(400, 100)) 
     self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
 
     self.verticalLayout = QVBoxLayout(self.verticalLayoutWidget)
@@ -50,7 +51,7 @@ class EntryWidget(QWidget):
     self.userName.setFixedWidth(300)
     self.userName.setObjectName("username")
     self.userName.setWordWrap(True)
-    self.userName.setText(entry["user"]["login"])
+    self.userName.setText(entry.user.login)
     self.userName.adjustSize()
     self.userName.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
     self.verticalLayout.addWidget(self.userName)
@@ -62,9 +63,11 @@ class EntryWidget(QWidget):
     self.message.setFixedWidth(300)
     self.message.setObjectName("message")
     self.message.setWordWrap(True)
-    self.message.setText(entry["text"])
+    self.message.setText(entry.text)
     self.message.adjustSize()
     self.message.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
+    # self.message.setTextInteractionFlags(QtCore.Qt.LinksAccessibleByMouse|QtCore.Qt.TextSelectableByMouse)
+    
 
 
     self.verticalLayout.addWidget(self.message)
@@ -79,7 +82,7 @@ class EntryWidget(QWidget):
     self.verticalLayout.addWidget(self.pushButton)
 
     self.verticalLayoutWidget.adjustSize()
-    self.entry.adjustSize()
+    self.entryWidget.adjustSize()
     self.adjustSize()
 
     self.setMinimumWidth(self.width())    
@@ -89,85 +92,182 @@ class EntryWidget(QWidget):
         self.button_click)
         
     self.clickStatus = None
+        
     
   def mouseReleaseEvent(self, event):
     if self.clickStatus == "double":
-      print "double"
-    self.clickStatus = None
+      self.emit(QtCore.SIGNAL("doubleClick(str)"), self.entry.id)
+
   
   def mouseDoubleClickEvent(self, event):
     self.clickStatus = "double"
     
   def button_click(event):
+    # del self.avatar
     print "click"
-
+  
+class LoginWidget(QWidget):
+  def __init__(self, flakerService, parent = None):
+    QWidget.__init__(self, parent)
+    
+    self.flakerService = flakerService
+    self.formLayoutWidget = QtGui.QWidget(self)
+    self.formLayoutWidget.setGeometry(QtCore.QRect(0, 20, 400, 200))
+    self.formLayoutWidget.setObjectName("loginWidget")
+    self.formLayout = QtGui.QFormLayout(self.formLayoutWidget)
+    self.formLayout.setObjectName("formLayout")
+    self.formLayout.setFieldGrowthPolicy(QtGui.QFormLayout.FieldsStayAtSizeHint)
+    self.usernameLabel = QtGui.QLabel(self.formLayoutWidget)
+    self.usernameLabel.setObjectName("usernameLabel")
+    self.usernameLabel.setText(u"Login")
+    self.formLayout.setWidget(1, QtGui.QFormLayout.LabelRole, self.usernameLabel)
+    self.usernameField = QtGui.QLineEdit(self.formLayoutWidget)
+    self.usernameField.setObjectName("usernameField")
+    self.formLayout.setWidget(1, QtGui.QFormLayout.FieldRole, self.usernameField)
+    self.passwordLabel = QtGui.QLabel(self.formLayoutWidget)
+    self.passwordLabel.setObjectName("passwordLabel")
+    self.passwordLabel.setText(u"Hasło")
+    self.formLayout.setWidget(2, QtGui.QFormLayout.LabelRole, self.passwordLabel)
+    self.passwordField = QtGui.QLineEdit(self.formLayoutWidget)
+    self.passwordField.setEchoMode(QtGui.QLineEdit.Password)
+    self.passwordField.setObjectName("passwordField")
+    self.formLayout.setWidget(2, QtGui.QFormLayout.FieldRole, self.passwordField)
+    self.loginButton = QtGui.QPushButton(self.formLayoutWidget)
+    self.loginButton.setObjectName("loginButton")
+    self.loginButton.setText("Zaloguj")
+    self.formLayout.setWidget(3, QtGui.QFormLayout.FieldRole, self.loginButton)
+    self.wrongCredentials = QtGui.QLabel(self.formLayoutWidget)
+    self.wrongCredentials.setObjectName("label")
+    self.wrongCredentials.setText(u"Złe dane")
+    self.wrongCredentials.hide()
+    self.formLayout.setWidget(0, QtGui.QFormLayout.LabelRole, self.wrongCredentials)
+    
+    self.connect(self.loginButton, QtCore.SIGNAL('clicked()'), self.login)
+    self.adjustSize()
+    
+    
+  def login(self):
+    self.flakerService.authorize(login=self.usernameField.text(), password=self.passwordField.text())
+    authorized = False
+    try:
+      authorized = self.flakerService.auth()
+    except:
+      authorized = False
+    
+    if authorized:
+      self.emit(QtCore.SIGNAL("loggedon"))
+    else: 
+      self.wrongCredentials.show()
+        
 class Ui_MainWindow(QMainWindow):
     def __init__(self, parent=None, flags=0):
         
         QMainWindow.__init__(self, parent)
-        # self.setObjectName("MainWindow")
-        self.resize(300, 500)
+        self.flakerService = Flaker()
+        
+        self.resize(400, 500)
         self.centralWidget = QWidget(self)
         self.centralWidget.setObjectName("centralWidget")
         self.setCentralWidget(self.centralWidget)
+        sizePolicy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Preferred)
+        self.setSizePolicy(sizePolicy)
      
         layout = QGridLayout(self.centralWidget)
+        layout.setMargin(0)
+        
+        
+        self.title = QLabel()
+        self.title.setMinimumWidth(400)
+        self.title.setText("FlaPy")
+        self.title.setObjectName("title")
+        self.title.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignCenter|QtCore.Qt.AlignTop)
+        layout.addWidget(self.title)
         
         self.scrollArea = QtGui.QScrollArea()
+        self.scrollArea.setGeometry(QtCore.QRect(0, 10, 400, 500))
         layout.addWidget(self.scrollArea)
-        layout.setMargin(0)
-        self.scrollArea.setGeometry(QtCore.QRect(0, 10, 300, 500))
         
         self.scrollArea.setWidgetResizable(True)
         self.scrollArea.setObjectName("scrollArea")
 
-        self.verticalLayoutWidget2 = QWidget()
-        self.scrollArea.setWidget(self.verticalLayoutWidget2)
+        self.createEntryListWidget()
+        self.loginWidget = LoginWidget(self.flakerService)
+        self.connect(self.loginWidget, QtCore.SIGNAL("loggedon"), self.login)
+        self.scrollArea.setWidget(self.loginWidget)
         
-        sizePolicy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
-        # sizePolicy.setHeightForWidth(self.sizePolicy().hasHeightForWidth())
-        self.verticalLayoutWidget2.setSizePolicy(sizePolicy)
-        # self.verticalLayoutWidget2.setGeometry(QtCore.QRect(10, 10, 500, 1000))
-        self.verticalLayoutWidget2.setSizeIncrement(QtCore.QSize(0, 10))
-        self.verticalLayoutWidget2.setBaseSize(QtCore.QSize(300, 100))
-        self.verticalLayoutWidget2.setObjectName("container")
-        self.verticalLayout2 = QVBoxLayout(self.verticalLayoutWidget2)
-        self.verticalLayout2.setSizeConstraint(QtGui.QLayout.SetMinimumSize)
-        self.verticalLayout2.setObjectName("verticalLayout2")
-        self.verticalLayout2.setMargin(0)
-        # self.widget = EntryWidget(self.centralWidget)
-        # self.widget.adjustSize()
-        # self.widget2 = EntryWidget(self.verticalLayoutWidget2)
-        # self.widget3 = EntryWidget(self.verticalLayoutWidget2)
-        # self.widget4 = EntryWidget(self.verticalLayoutWidget2)
-        # 
-        # self.verticalLayout2.addWidget(self.widget)
-        # self.verticalLayout2.addWidget(self.widget2)
-        # self.verticalLayout2.addWidget(self.widget3)
-        # self.verticalLayout2.addWidget(self.widget4)
-        # 
-        # self.verticalLayoutWidget2.adjustSize()
-        # self.scrollArea.setWidget(self.verticalLayoutWidget2)
-        # self.setCentralWidget(self.scrollArea)
-        self.entrywidgets = []
-        
-        entries = flaker.getEntries("piotrj")
-        for entry in entries["entries"]:
-          widget = EntryWidget(entry, self.verticalLayoutWidget2)
-          self.entrywidgets.append(widget)
-          self.verticalLayout2.addWidget(widget)
-          
-        
-        self.verticalLayoutWidget2.adjustSize()
-        self.centralWidget.setFixedWidth(self.centralWidget.sizeHint().width()+15)
-        self.centralWidget.setMaximumWidth(self.centralWidget.width())
+
         
         f = open("entry.css", "r")
         stylesheet = f.read()
         f.close
         self.setStyleSheet(stylesheet)
+        
 
+    def createEntryListWidget(self):
+      self.entryListContainer = QWidget()
+      sizePolicy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+      self.entryListContainer.setSizePolicy(sizePolicy)
+      self.entryListContainer.setSizeIncrement(QtCore.QSize(0, 10))
+      self.entryListContainer.setBaseSize(QtCore.QSize(300, 100))
+      self.entryListContainer.setObjectName("container")
+      self.entryListLayout = QVBoxLayout(self.entryListContainer)
+      self.entryListLayout.setSizeConstraint(QtGui.QLayout.SetMinimumSize)
+      self.entryListLayout.setObjectName("entryListLayout")
+      self.entryListLayout.setMargin(0)
 
+      self.entrywidgets = []
+
+      # entries = flakerService.getNewEntries("piotrj")
+      entries = self.flakerService.friends("piotrj", 1)
+      
+      for entry in entries:
+        widget = EntryWidget(entry, self.entryListContainer)
+        self.entrywidgets.append(widget)
+        self.entryListLayout.addWidget(widget)
+        self.connect(widget, QtCore.SIGNAL("doubleClick(str)"), self.doubleClick)
+        
+      self.entryListContainer.adjustSize()
+
+  
+    def createEntryDetailWidget(self, id):
+      self.detailContainer = QWidget()
+      sizePolicy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+      self.detailContainer.setSizePolicy(sizePolicy)
+      self.detailContainer.setSizeIncrement(QtCore.QSize(0, 10))
+      self.detailContainer.setBaseSize(QtCore.QSize(400, 100))
+      self.detailContainer.setObjectName("container")   
+      self.detailLayout = QVBoxLayout(self.detailContainer)
+      self.detailLayout.setSizeConstraint(QtGui.QLayout.SetMinimumSize)
+      self.detailLayout.setObjectName("detailLayout")
+      self.detailLayout.setMargin(0)
+
+      entry = self.flakerService.show(id)
+
+      self.detailwidgets = []
+      widget = EntryWidget(entry, self.detailContainer)
+      self.detailwidgets.append(widget)
+      self.detailLayout.addWidget(widget)
+
+      for comment in entry.comments:
+        widget = EntryWidget(comment, self.detailContainer)
+        self.detailwidgets.append(widget)
+        self.detailLayout.addWidget(widget)   
+        
+    def doubleClick(self, id):
+      self.createEntryDetailWidget(id)
+      self.entryListContainer.hide()
+      self.scrollArea.setWidget(self.detailContainer)
+    
+    def login(self):
+      self.createEntryListWidget()
+      self.scrollArea.setWidget(self.entryListContainer)
+      self.entryListContainer.adjustSize()
+      del self.loginWidget
+      
+      self.centralWidget.adjustSize()
+      self.centralWidget.setFixedWidth(self.centralWidget.sizeHint().width()+15)
+      self.centralWidget.setMaximumWidth(self.centralWidget.width())
+        
 if __name__ == "__main__": 
   app = QApplication(sys.argv) 
   window = Ui_MainWindow() 
